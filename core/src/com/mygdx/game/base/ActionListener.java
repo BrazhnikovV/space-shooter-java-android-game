16ac19d9946f98c36c744cc23808194a63ef03d5
BrazhnikovV <brazhnikov_ve@mail.ru>
2018-11-01 21:05:26 +0300

    Докблок для интерфейса ActionListener.

--- core/src/com/mygdx/game/base/ActionListener.java
+++ core/src/com/mygdx/game/base/ActionListener.java
@@ -1,5 +1,14 @@
 package com.mygdx.game.base;
 
+/**
+ * ActionListener - интерфейс
+ *
+ * @version 1.0.1
+ * @package com.mygdx.game.base
+ * @author  Vasya Brazhnikov
+ * @copyright Copyright (c) 2018, Vasya Brazhnikov
+ */
 public interface ActionListener {
-    void actionPerformed(Object src);
+
+    void actionPerformed( Object src );
 }
